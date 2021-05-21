647c64ca9f369a9f0e3fd6c140e95f18496bd25b
Demon <demonch@163.com>
2021-05-21 09:37:39 +0800

    commit test by demon

--- run_v6.bat
+++ run_v6.bat
@@ -1,8 +1,8 @@
 
 
 @echo off&setlocal enabledelayedexpansion
-set startDate=2021/05/31
-set end=2
+set startDate=2021/05/21
+set end=6
 set yyyy=%startDate:~0,4%
 set mm=%startDate:~5,2%
 set dd=%startDate:~8,2%
