71cbb170fa8d2b934578d468ad4ba7e85df758d2
chenhao <demonch@163.com>
2022-12-16 13:55:16 +0800

    commit test by demon

--- run_v6.bat
+++ run_v6.bat
@@ -1,8 +1,8 @@
 
 
 @echo off&setlocal enabledelayedexpansion
-set startDate=2022/12/9
-set end=4
+set startDate=2022/12/16
+set end=5
 set yyyy=%startDate:~0,4%
 set mm=%startDate:~5,2%
 set dd=%startDate:~8,2%
